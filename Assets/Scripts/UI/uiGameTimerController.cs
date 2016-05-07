0b96f64b5e2270d8a7a1ca36844e898067af7947
Jamison Fullerton <zelda.jdf@gmail.com>
2016-05-07 18:49:57 -0400

    Convertin Initial UI to prefab

---
+++ Assets/Scripts/UI/uiGameTimerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class uiGameTimerController : MonoBehaviour {
+
+    public Text textComponent;
+
+    public float time;
+    private bool timeStarted;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(timeStarted)
+        {
+            time -= Time.deltaTime;
+            if(time < 0)
+            {
+                timeStarted = false;
+                // fire times up event
+            }
+        }
+	}
+
+    void SetLevelTime(float _time)
+    {
+        time = _time;
+    }
+
+    void StartTimer(bool _start = true)
+    {
+        timeStarted = _start;
+    }
+}
