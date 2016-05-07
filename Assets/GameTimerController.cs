a0999fcc4a5a6d9b9afe15b973f81d0516374fe3
Jamison Fullerton <zelda.jdf@gmail.com>
2016-05-07 17:37:16 -0400

    Initial UI development

---
+++ Assets/GameTimerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameTimerController : MonoBehaviour {
+
+    public Text textComponent;
+
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
