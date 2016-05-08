e7476e6f35f385c14f61aff52aad28f9643ccdd6
Chris Eva <Chris Eva>
2016-05-07 23:00:43 -0400

    Sounds now playing on player + Animation Sound Trigger system - Added Sounds to player

---
+++ Assets/Scripts/ActorSounds.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+[System.Serializable()]
+public class AudioDefinitions : SerializableDictionaryBase<string, AudioClip> { }
+
+public class ActorSounds : MonoBehaviour {
+
+    [SerializeField()]
+    public AudioDefinitions m_eventClips;
+    private Dictionary<string, AudioSource> m_audioSources;
+	// Use this for initialization
+	void Start () {
+        m_audioSources = new Dictionary<string, AudioSource>();
+
+        foreach (var pairIt in m_eventClips)
+        {
+            m_audioSources[pairIt.Key] = gameObject.AddComponent<AudioSource>();
+            m_audioSources[pairIt.Key].clip = pairIt.Value;
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void TriggerSound(string eventName)
+    {
+        m_audioSources[eventName].Play();
+    }
+}
